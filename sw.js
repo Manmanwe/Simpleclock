b0e5992f9fcb65e70f6b8b5e93d46b2623954dd4
Manmanwe <143009370+Manmanwe@users.noreply.github.com>
2026-09-19 11:46:45 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+// A simple Service Worker to cache the files and make it work offline
+const CACHE_NAME = 'switch-dash-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
